4a98622885009d548b0d094666c7d524c1846cf2
simulcast <thisissimulcast@gmail.com>
2017-03-09 13:36:35 -0500

    first draft
    initial test!

---
+++ index.js
@@ -0,0 +1,95 @@
+/* where i need to go from here:
+- have server store state of program, so that when you log on everything gets called up correctly (opacity states etc) / starts on beat
+- blinking before sound is played / stopped but hasn't yet executed?
+*/
+
+var express = require('express');  
+var app = express();  
+var server = require('http').createServer(app);  
+var io = require('socket.io')(server);
+
+app.use(express.static('public')); 
+var totalUsers = 0,
+    stepID = 0,
+    friendsGroup = [];
+
+io.sockets.on('connection', function (socket) {
+  // new id
+  var thisID = getID();
+  // step users++
+  addUser();
+  // new connection ALL
+  io.sockets.emit('connected', { connections: totalUsers });
+  // new connection friends
+  socket.broadcast.emit('new friend', { friend: thisID  });
+  // new connection self
+  socket.emit('init',{ player:thisID, friends: friendsGroup });
+  // disconnect friends
+  socket.on('disconnect', function (){
+      removeUser(thisID);
+      socket.broadcast.emit('bye friend',{connections:totalUsers, friend: thisID});
+  });
+  // mouse move
+  socket.on('move',function(data){
+      socket.broadcast.emit('move', data);
+  });
+  //console.log(friendsGroup);
+});
+
+// Functions
+
+function getID() {
+    friendsGroup.push(++stepID);
+    return stepID;
+}
+
+function addUser(){
+    totalUsers++;
+}
+
+function removeUser(thisID){
+    friendsGroup = removeFromArray(thisID,friendsGroup);
+    totalUsers--;
+}
+
+// Helpers
+
+function removeFromArray(string, array) {
+  var i = 0;
+  for(i in array){
+    if(array[i] === string){
+      array.splice(i,1);
+    }
+  }
+  return array;
+}
+
+var togglestate = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,];
+
+io.on('connection', function(socket){
+	/* takes in number and sends it to appropriate soundfile */
+	socket.on('playtoggle', function(number) {
+		if (togglestate[number] == 0) {
+	  	console.log('play ' + number);
+	  	io.emit('play', number);
+			togglestate[number]++;
+		}
+		else if (togglestate[number] == 1) {
+			console.log('stop '  + number);
+			io.emit('stop', number);
+			togglestate[number] = 0;
+		};
+	});
+
+  socket.on('disconnect', function(){
+	console.log('user disconnected');
+  });
+});
+
+app.get('/', function(req, res,next) {  
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(3000, function(){
+  console.log('listening on *:3000');
+});
---
+++ public/js/script.js
@@ -0,0 +1,63 @@
+var socket = io();
+
+$(document).ready(function() {
+	var sounds = []
+	//tone.js transport and sound loading
+	Tone.Transport.bpm.value = 127;
+	Tone.Transport.start();
+
+	var bmore = new Tone.Player({
+		"url": "../sound/bmore.wav",
+		"autostart": false,
+		"loop": false
+	}).toMaster();
+	sounds.push(bmore);
+
+	var beep = new Tone.Player({
+		"url": "../sound/beep.wav",
+		"autostart": false,
+		"loop": false
+	}).toMaster();
+	sounds.push(beep); //put object at the end of the sounds array
+
+	var boop = new Tone.Player({
+		"url": "../sound/boop.wav",
+		"autostart": false,
+		"loop": false
+	}).toMaster();
+	sounds.push(boop); //put object at the end of the sounds array
+
+	console.log(sounds);
+
+	/* takes in clicks and emits the id of the box clicked */
+
+	$(".box").each(function(index) {
+	    $(this).on("click", function(){
+	    	//takes the last number of box__ and sends it through socket.io
+	        var id = $(this).attr('id').substring(3);
+	        console.log(id); 
+			socket.emit('playtoggle', id)
+	    });
+	});
+	/* takes in signal to play and plays the corresponding sound file */
+	socket.on('play', function(number){
+		sounds[number].start("@1n");
+		sounds[number].loop = true;
+		$("#box" + number).css('opacity', '0.5');
+	});
+
+	/* takes in signal to stop and stops the corresponding sound file */
+	socket.on('stop', function(number){
+	  	sounds[number].stop("@1n");
+	  	sounds[number].loop = false;
+		$("#box" + number).css('opacity', '1.0');
+	});
+
+	/* beat counter for log file */
+	var loop = new Tone.Loop(function(time){
+	   console.log('beat #');
+	}, "4n");
+
+	//loop between the first and fourth measures of the Transport's timeline
+	loop.start();
+});
