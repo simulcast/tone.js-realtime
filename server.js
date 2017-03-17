86fd71e0f98f25f98d39093a2c45f6844fbd3bbd
simulcast <thisissimulcast@gmail.com>
2017-03-17 11:02:21 -0400

    fixed case

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express');
 var app = express();  
 var server = require('http').createServer(app);  
 var io = require('socket.io')(server);
-var Repeat = require('Repeat');
+var Repeat = require('repeat');
 var $ = require("jquery");
 
 var bpm = 127; //bpm is 127
